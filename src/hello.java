2f1564daeb3f9e41491d31a02358aad9a3451867
unknown <smitdept@naver.com>
2019-08-01 14:53:04 +0900

    git test

---
+++ src/hello.java
@@ -0,0 +1,16 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte a = 10;
+		byte b = 20;
+		byte c;
+		
+		System.out.println(a);
+	
+
+	}
+
+}
